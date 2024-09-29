62172867f710fb7f74ae6c85dbd08ca4ee4a2155
akshay-1231 <akshayprajapati424206@gmail.com>
2024-09-29 14:31:40 +0530

    Update ArrayFibo.java

--- ArrayFibo.java
+++ ArrayFibo.java
@@ -4,15 +4,22 @@ public class ArrayFibo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      int[]arr=new int[5];
-      arr[0]=0;
-      arr[1]=1;
-      System.out.println(arr[0]+" "+arr[1]);
-      for(int i=2;i<arr.length-1;i++)
-      {
-    	  arr[i]=arr[i-1]+arr[i+2];
-    	  
-      }     System.out.println(" "+arr[i])
+                int[] arr = new int[5]; //       Array size   5
+        arr[0] = 0;  // First Fibo num
+        arr[1] = 1;  // Second Fibon num
+        
+        
+        System.out.println(arr[0] + " " + arr[1]);
+        
+        
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];  
+        }
+        
+        
+        for (int i = 2; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");  
+        }
 	}
 
 }
